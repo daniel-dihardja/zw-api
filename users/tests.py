84b55d55f1fff7f6c24cbcd22e2204b5c840c040
Daniel Dihardja <daniel@dihardja.de>
2024-07-20 08:51:07 +0200

    add pytest

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_create_user():
+    user = User.objects.create_user(username="testuser", password="testpassword")
+    assert user.username == "testuser"
