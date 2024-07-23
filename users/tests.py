91297812b0e543660f0a22305901aadbd9122b5c
Daniel Dihardja <daniel@dihardja.de>
2024-07-23 16:30:38 +0200

    remove pytest

--- users/tests.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.contrib.auth.models import User
-
-
-@pytest.mark.django_db
-def test_create_user():
-    user = User.objects.create_user(username="testuser", password="testpassword")
-    assert user.username == "testuser"
